04adaf6e9dafb1210e4fe73300cd01618fe2d01b
ThirdPartyHugo <201945297@cegepsherbrooke.qc.ca>
2024-11-24 20:14:16 -0500

    ff

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import LoginForm  from './LoginForm';
-import SignupForm from './SignupForm';
+import { LoginForm } from './LoginForm';
+import { SignupForm } from './SignupForm';
 
 export function AuthContainer() {
   const [isLogin, setIsLogin] = useState(true);
